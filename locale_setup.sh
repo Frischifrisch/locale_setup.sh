f6c4c33f408d088d24728905eaac77e227c5c6d4
Christian <christian_frischholz@web.de>
2024-01-22 20:51:25 +0100

    Create locale_setup.sh

---
+++ locale_setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# FILEPATH: locale_setup.sh
+
+# Disable Read-Only mode
+sudo steamos-readonly disable
+
+# Init Pacman Keys
+sudo pacman-key --init
+sudo pacman-key --populate
+sudo pacman-key --refresh-keys
+
+# Edit locale.gen
+# sudo nano /etc/locale.gen
+echo 'de.DE.UTF-8 UTF-8' >> /etc/locale.gen
+
+# fetch glibc with the needed locales
+sudo pacman -S glibc
+
+# generate locales
+sudo locale-gen
+
+# install i18n package for kde
+sudo pacman -S ki18n
+
+# reinstall plasma packages with language support
+sudo pacman -S plasma
+
+# install german spelling tools
+sudo pacman -S aspell-it
+
+# reinstall all packages
+sudo pacman -Qqn | sudo pacman -S -
+
+# reboot system
+sudo shutdown -r now
